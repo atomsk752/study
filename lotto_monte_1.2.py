50b7a91ebd4a2cae7b4d860e6f710a69f48f3217
atomsk752 <40642802+atomsk752@users.noreply.github.com>
2018-08-02 20:27:22 +0900

    Update lotto_monte_1.2.py
    함수를 이용하여 다회차로 사용

--- lotto_monte_1.2.py
+++ lotto_monte_1.2.py
@@ -1,23 +1,36 @@
 from random import randint
-arr=[randint(1, 45) for x in range(1000)]
 
-result = [{"name": x, "count" : 0} for x in range(1, 46)]
+def lotto_cal():
+    arr=[randint(1, 45) for x in range(1000)]
 
-for num in arr:
-    result[num - 1]["count"] += 1
+    result = [{"name": x, "count" : 0} for x in range(1, 46)]
 
-lotto=[]
+    for num in arr:
+        result[num - 1]["count"] += 1
 
-for k in range(6):
-    max = 0
-    max_index = 0
+    lotto=[]
 
-    for idx, x in enumerate(result):
+    for k in range(6):
+        max = 0
+        max_index = 0
 
-        if x['count'] > max:
-            max = x['count']
-            max_index = idx
+        for idx, x in enumerate(result):
 
-    lotto.append(result.pop(max_index)['name'])
+            if x['count'] > max:
+                max = x['count']
+                max_index = idx
 
-print(lotto)
+        lotto.append(result.pop(max_index)['name'])
+    return lotto
+
+
+x = int(input("몇 회차를 시도하시겠습니까?: "))
+lotto_time = 0
+while lotto_time < x:
+    lotto_time += 1
+    print("%d회차 번호" % lotto_time)
+    print(lotto_cal())
+    if lotto_time == x:
+        print("출력완료.")
+
+
