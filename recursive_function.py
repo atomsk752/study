8110920c90157634921dec20af410b1d7ee313e2
atomsk752 <40642802+atomsk752@users.noreply.github.com>
2018-08-03 18:30:13 +0900

    Add files via upload
    이진수 알고리즘

---
+++ recursive_function.py
@@ -0,0 +1,11 @@
+#2진수 계산 재귀함수
+def convert (value):
+    odd = value % 2
+
+    if int(value / 2) == 0:
+        return "1"
+
+    return str(convert(int(value/2))) + str(odd)
+
+for x in range(1, 30):
+    print(convert(x))
